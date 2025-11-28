e7d301ca38210b285dece59022d1ed4e626b0122
MentynskyiSM <serhii.m.mentynskyi@lpnu.ua>
2025-11-28 11:52:23 +0200

    Pas 600 final

--- Pascal/Task600.pas
+++ Pascal/Task600.pas
@@ -25,7 +25,8 @@ begin
         read(database,data);
         writeln(data.author,'  ',data.book_title,
         ' ', data.publisher,' ',data.publish_year);
-    end    
+    end;
+    close(database);
 end;
 
 procedure find_by_author(filename: string);
@@ -43,7 +44,8 @@ begin
         if pos(author, data.author) > 0 then
             writeln(data.author,'  ',data.book_title,
             ' ', data.publisher,' ',data.publish_year);
-    end    
+    end;
+    close(database);
 end;
 
 procedure find_book(filename: string);
@@ -61,8 +63,10 @@ begin
         if pos(book_title, data.book_title) > 0 then
             writeln(data.author,'  ',data.book_title,
             ' ', data.publisher,' ',data.publish_year);
-    end    
+    end;
+    close(database);
 end;
+
 procedure read_by_publisher(filename: string);
     var data: book;
     publisher : string[50]; {локальна змінна для пошуку 
@@ -77,7 +81,67 @@ begin
         if data.publisher = publisher then
         writeln(data.author,'  ',data.book_title,
         ' ', data.publisher,' ',data.publish_year);
-    end    
+    end;
+    close(database);
+end;
+
+function inputbook() : book;
+    var result : book;
+begin
+    writeln('Введіть про нову книгу');
+    write('Автор(автори):'); readln(result.author);
+    write('Назва:'); readln(result.book_title);
+    write('Видавництво:'); readln(result.publisher);
+    write('Рік видання:'); readln(result.publish_year);
+    write('Ціна:'); readln(result.price);
+    write('Кількість:'); readln(result.count);
+    inputbook := result;
+end;
+
+procedure writenewfile(filename: string);
+var choise: integer; 
+begin
+    assign(database, filename);
+    rewrite(database);
+    repeat
+    tmp := inputbook();  
+    write(database, tmp);
+    writeln('Додати ще одну книгу?');
+    writeln('1 - так, додати ще;');
+    writeln('0 - зберегти дані і вийти');
+    readln(choise);
+    until(choise<>1);
+    writeln('нову базу книг стоврено');
+    close(database);
+end;
+
+procedure writebook(filename: string; data: book);
+const tmp_filename = 'database.dump';
+var dump_file: file of book;
+begin
+    assign(database, filename);
+    reset(database);
+    assign(dump_file, tmp_filename);
+    rewrite(dump_file);
+    while eof(database)=false do
+    begin
+        read(database,tmp);
+        write(dump_file,tmp)
+    end;
+    
+    write(dump_file, data);
+    close(dump_file);
+    rewrite(database);
+    reset(dump_file);
+    while eof(dump_file)=false do
+    begin
+        read(dump_file,tmp);
+        write(database,tmp)
+    end;
+    writeln('книга додана в базу');
+    close(database);
+    close(dump_file);
+    deletefile(tmp_filename)
 end;
 
 begin
@@ -86,7 +150,9 @@ begin
     writeln('1 - шукати книгу за назвою.');
     writeln('2 - шукати книги за прізвищем автора.');
     writeln('3 - вивести книги вказаного видавництва');
-    writeln('4 - перегляднути всі книги в базі');
+    writeln('4 - переглянути всі книги в базі');
+    writeln('5 - створити/перестворити базу книг.');
+    writeln('6 - дописати нову книгу в базу');
     writeln('0 - завершити роботу');
     readln(choise);
     case (choise) of
@@ -94,6 +160,8 @@ begin
       2:find_by_author(db_name);
       3:read_by_publisher(db_name);
       4:readall(db_name);
+      5:writenewfile(db_name);
+      6:writebook(db_name, inputbook());
     end;
   until(choise = 0)
 end.
